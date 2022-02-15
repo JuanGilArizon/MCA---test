7b641db4a97cad3a645dfbb9d72ebc488b80a40d
Juan Gil <juangilarizon@gmail.com>
2022-02-15 20:15:58 +0100

    WIP search funcionality

--- src/pages/ProductsListPage/ProductsListPage.js
+++ src/pages/ProductsListPage/ProductsListPage.js
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ItemList from '../../components/ItemList/ItemList';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 function ProductsListPage() {
   const navigate = useNavigate();
+  const [data] = useState([{ id: '1', name: 'one' }, { id: '2', name: 'two' }]);
+  const [filteredData, setFilteredData] = useState(data);
   const handleItemClick = (itemId) => {
     navigate(itemId);
   };
+  const filterData = (textCriteria) => {
+    // TODO: comparar con marca y modelo
+    const filtered = data.filter((product) => product.name.includes(textCriteria));
+    setFilteredData(filtered);
+  };
   return (
-    <ItemList data={[{ id: '1', name: 'one' }]} onItemClick={handleItemClick} />);
+    <>
+      <SearchBar onTextChange={filterData} />
+      <ItemList data={filteredData} onItemClick={handleItemClick} />
+    </>
+  );
 }
 export default ProductsListPage;
