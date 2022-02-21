93e2a386ccc6a6ad75016bfb8c27f8efc102ff87
Juan Gil <juangilarizon@gmail.com>
2022-02-21 23:04:41 +0100

    style changes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import './searchBar.scss';
 
 function SearchBar({ className, onTextChange }) {
   return (
-    <div className={className}>
-      <label htmlFor="searchBar">
+    <div className={`${className} mr-2`}>
+      <label className="search-label" htmlFor="searchBar">
         Search
         <input id="searchBar" type="text" className="search-bar" onChange={(e) => onTextChange(e.target.value)} />
       </label>
